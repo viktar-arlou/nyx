a157ac4e8383f72fea4d026751b117b1808aea35
Arvik <varlou@gmail.com>
2014-09-05 15:53:37 +0200

    more factory methods

--- src/main/java/nyx/collections/Acme.java
+++ src/main/java/nyx/collections/Acme.java
@@ -1,9 +1,11 @@
 package nyx.collections;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -19,12 +21,15 @@ public class Acme {
 	public static ByteBuffer dbbuffer(int capacity) {return ByteBuffer.allocateDirect(capacity);}
 	public static byte[] abyte(int size) {return new byte[size];}
 	public static <K,V> Map<K, V> hashmap() {return new HashMap<>();}
-	public static <K> Set<K> hashset() {return new HashSet<>();}
-	public static <K> Set<K> hashset(int capacity) {return new HashSet<>(capacity);}
-	public static <K> Set<K> hashset(Set<K> set) {return new HashSet<>(set);}
-	public static <K> Set<K> chashset() {return Collections.newSetFromMap(new ConcurrentHashMap<K,Boolean>());}
-	public static <K> Set<K> chashset(int capacity) {return Collections.newSetFromMap(new ConcurrentHashMap<K,Boolean>(capacity));}
+	public static <E> Set<E> hashset() {return new HashSet<>();}
+	public static <E> Set<E> hashset(int capacity) {return new HashSet<>(capacity);}
+	public static <E> Set<E> hashset(Set<E> set) {return new HashSet<>(set);}
+	public static <E> Set<E> copy(Set<E> set) {return new HashSet<>(set);}
+	public static <E> List<E> copy(List<E> set) {return new ArrayList<>(set);}
+	public static <E> Set<E> chashset() {return Collections.newSetFromMap(new ConcurrentHashMap<E,Boolean>());}
+	public static <E> Set<E> chashset(int capacity) {return Collections.newSetFromMap(new ConcurrentHashMap<E,Boolean>(capacity));}
 	public static <K,V> Map<K, V> chashmap() {return new ConcurrentHashMap<>();}
-	public static <K> Set<K> umset(Set<K> set) {return Collections.unmodifiableSet(set);}
-	
+	public static <E> Set<E> umset(Set<E> set) {return Collections.unmodifiableSet(set);}
+	public static <E> List<E> alist() {return new ArrayList<>();}
+	public static <E> List<E> alist(int capacity) {return new ArrayList<>(capacity);}
 }
